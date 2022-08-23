642be79a5fbe572abec1153b7d961630a44c47d1
Tigran <tigran2589@gmail.com>
2022-08-23 20:50:14 +0300

    Finished project

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -3,8 +3,7 @@ import AppContext from '../context'
 
 export const useCart = () => {
   const { cartItems, setCartItems } = React.useContext(AppContext)
-  console.log(cartItems)
-  const totalPrice = cartItems.reduce((sum, obj) => obj.price + sum, 0)
+  const totalPrice = cartItems.reduce((sum, obj) => Number(obj.price) + Number(sum), 0)
 
   return { cartItems, setCartItems, totalPrice }
 }
